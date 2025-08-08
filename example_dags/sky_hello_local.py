5c112c98f489e0bca2f7e593c571ed80b057309e
Kevin Mingtarja <kevin.mingtarja@gmail.com>
2025-08-08 12:57:56 -0700

    move example_dags dir

---
+++ example_dags/sky_hello_local.py
@@ -0,0 +1,28 @@
+import pendulum
+from airflow import decorators
+
+from sky_provider import operators
+
+default_args = {
+    "owner": "airflow",
+    "retries": 1,
+}
+
+
+@decorators.dag(default_args=default_args,
+                start_date=pendulum.datetime(2023, 1, 1),
+                catchup=False,
+                tags=["skypilot"])
+def sky_hello_local():
+    hello_task = operators.SkyTaskOperator(
+        task_id="hello_task",
+        # If running locally, copy the example_skypilot_yamls/ directory by doing:
+        # docker compose cp ./example_skypilot_yamls/ airflow-worker:/opt/airflow/
+        base_path="/opt/airflow",
+        yaml_path="example_skypilot_yamls/hello.sky.yaml",
+    )
+
+    hello_task
+
+
+sky_hello_local()
